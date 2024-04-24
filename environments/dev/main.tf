1cf29ef439c1ae24c56563ba5febb7fb9e3615d4
Sandeep Agarwal <pensande@gmail.com>
2024-04-24 15:39:30 +0000

    using google-beta provider

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -827,6 +827,7 @@ resource "google_project_iam_member" "cc_workload_user" {
 }
 
 resource "google_iam_workload_identity_pool_provider" "primus_pool_provider" {
+  provider                           = google-beta
   workload_identity_pool_id          = "${module.primus_services.pool_id}"
   workload_identity_pool_provider_id = "${var.primus_project}-provider"
   display_name                       = "${var.primus_project}-provider"
@@ -842,6 +843,7 @@ resource "google_iam_workload_identity_pool_provider" "primus_pool_provider" {
 }
 
 resource "google_iam_workload_identity_pool_provider" "secundus_pool_provider" {
+  provider                           = google-beta
   workload_identity_pool_id          = "${module.secundus_services.pool_id}"
   workload_identity_pool_provider_id = "${var.secundus_project}-provider"
   display_name                       = "${var.secundus_project}-provider"
