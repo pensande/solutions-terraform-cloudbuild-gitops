081f7d5c27511430cdb54127015de2914899b040
Sandeep Agarwal <pensande@gmail.com>
2024-04-24 13:06:16 +0000

    updating google provider version for the cc module

--- modules/cc_setup/versions.tf
+++ modules/cc_setup/versions.tf
@@ -1,3 +1,9 @@
 terraform {
-  required_version = "~> 1.0.0"
+  required_version = ">= 0.13"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 3.53, < 5.0"
+    }
+  }
 }
