8de53bf86fb7202e8f159e393bf36dfce05a28d8
Sandeep Agarwal <pensande@gmail.com>
2024-06-28 16:12:45 +0000

    getting rid of vpc module

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -17,5 +17,10 @@ variable "env" {}
 variable "region" {}
 variable "secondary_ranges" {
     default = null
-    type = map
+    type = list(object(
+        {
+            event_type  = string
+            resource    = string
+        }
+    ))
 }
