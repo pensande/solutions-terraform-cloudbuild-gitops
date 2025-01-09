d47d8847cc6edf344a5065a85095c695153f73a3
Sandeep Agarwal <pensande@gmail.com>
2025-01-09 07:11:21 +0000

    both workloads to run aws container

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1000,7 +1000,7 @@ resource "google_compute_instance" "first_workload_cvm" {
   }
   
   metadata = {
-    tee-image-reference = "${var.region}-docker.pkg.dev/${var.primus_project}/${module.primus_services.repo_name}/workload-container:latest"
+    tee-image-reference = "${var.region}-docker.pkg.dev/${var.primus_project}/${module.primus_services.repo_name}/awsdemo-container:latest"
     tee-restart-policy  = "Never"
     tee-cmd             = "[\"count-location\",\"Seattle\",\"gs://${google_storage_bucket.result_bucket.name}/seattle-result\"]"
   }
